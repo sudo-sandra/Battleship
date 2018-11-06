774dfb27b4a4b812f0d24f68b849daaa28f9df97
Seger Sandra <sandra.seger@frm2.tum.de>
2018-11-06 15:58:15 +0100

    improve set up map

--- app/src/main/java/com/example/root/battleship/SetUpMap.java
+++ app/src/main/java/com/example/root/battleship/SetUpMap.java
@@ -1,11 +1,14 @@
 package com.example.root.battleship;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class SetUpMap extends AppCompatActivity implements View.OnClickListener{
 
@@ -16,6 +19,25 @@ public class SetUpMap extends AppCompatActivity implements View.OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.set_up_map);
+        TableRow.LayoutParams layout_params = new TableRow.LayoutParams(50, 50);
+        layout_params.setMargins(5,5,5,5);
+        View[] ships = new View[10];
+        for (int i = 0; i < 10; i++) {
+            ships[i] = new View(this);
+            ships[i].setLayoutParams(layout_params);
+            ships[i].setBackgroundColor(Color.RED);
+        }
+        TableLayout legend = (TableLayout) findViewById(R.id.legendLayout);
+        int count = 0;
+        for(int i = 0; i < 4 ; i++){
+            TableRow row = (TableRow) legend.getChildAt(i);
+            TextView txt = (TextView) row.getChildAt(0);
+            txt.setTextSize(28);
+            for(int k = Math.abs(i - 4); k > 0; k--){
+                row.addView(ships[count]);
+                count++;
+            }
+        }
         Button auto_btn = (Button)findViewById(R.id.auto_btn);
         auto_btn.setOnClickListener(this);
         Button ok_btn = (Button)findViewById(R.id.ok_btn);
