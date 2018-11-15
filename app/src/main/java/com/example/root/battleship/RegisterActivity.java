3f112fe48e37ffec8562740fa87dfbd61e91a052
Jana <jana_egge@gmx.de>
2018-11-15 12:03:36 +0100

    name changes + DB insert

--- app/src/main/java/com/example/root/battleship/RegisterActivity.java
+++ app/src/main/java/com/example/root/battleship/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -17,7 +18,15 @@ public class RegisterActivity extends AppCompatActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.registerBtn:
-                openGameMenuActivity();
+                EditText usernameTxtField = (EditText) findViewById(R.id.usernameRegisterField);
+                EditText passwordTxtField = (EditText) findViewById(R.id.passwordRegisterField);
+                EditText repasswordTxtField = (EditText) findViewById(R.id.repasswordRegisterField);
+                if (passwordTxtField.equals(repasswordTxtField)) {
+                    DBConnection connection = new DBConnection();
+                    connection.openConnection();
+                    connection.insertUserData(usernameTxtField.toString(), passwordTxtField.toString());
+                    openGameMenuActivity();
+                }
                 break;
         }
     }
