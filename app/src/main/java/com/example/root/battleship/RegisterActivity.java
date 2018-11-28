9523bd6a2e439d1b7ca2d19b7e2d50c5e740841c
Jana <jana.eggert@tum.de>
2018-11-28 09:59:58 +0100

    implementing insert of userdata into firebase db

--- app/src/main/java/com/example/root/battleship/RegisterActivity.java
+++ app/src/main/java/com/example/root/battleship/RegisterActivity.java
@@ -3,9 +3,9 @@ package com.example.root.battleship;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -13,17 +13,20 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
     }
 
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.registerBtn:
+
                 EditText usernameTxtField = (EditText) findViewById(R.id.usernameRegisterField);
                 EditText passwordTxtField = (EditText) findViewById(R.id.passwordRegisterField);
                 EditText repasswordTxtField = (EditText) findViewById(R.id.repasswordRegisterField);
-                if (passwordTxtField.equals(repasswordTxtField)) {
+
+                if (passwordTxtField.getText().toString().equals(repasswordTxtField.getText().toString())) {
                     DBConnection connection = new DBConnection();
-                    connection.insertNewUser(usernameTxtField.toString(), passwordTxtField.toString());
+                    connection.insertNewUser(usernameTxtField.getText().toString(), passwordTxtField.getText().toString());
                     openGameMenuActivity();
                 }
                 break;
