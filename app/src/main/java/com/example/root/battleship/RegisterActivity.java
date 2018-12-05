e94afad58cd4d787b4a21ea0a24de8aedf0843cd
Jana <jana.eggert@tum.de>
2018-12-05 15:56:30 +0100

    changed login logic

--- app/src/main/java/com/example/root/battleship/RegisterActivity.java
+++ app/src/main/java/com/example/root/battleship/RegisterActivity.java
@@ -1,11 +1,17 @@
 package com.example.root.battleship;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -19,17 +25,10 @@ public class RegisterActivity extends AppCompatActivity {
         switch (view.getId()) {
             case R.id.registerBtn:
 
-                EditText usernameTxtField = findViewById(R.id.usernameRegisterField);
-                EditText passwordTxtField = findViewById(R.id.passwordRegisterField);
-                EditText repasswordTxtField = findViewById(R.id.repasswordRegisterField);
-
-                String username = usernameTxtField.getText().toString();
-                String password = passwordTxtField.getText().toString();
-                String repassword = repasswordTxtField.getText().toString();
-
+                User user = readInputFields();
                 //Checking Validity of Username and Password, if true insert User into Database and forward User to Game Menu
-                if(checkingUsernameOnValidity(DBConnection.getInstance(), username) & checkingPasswordOnValidity(password, repassword)) {
-                    DBConnection.getInstance().insertUserIntoDB(username, password);
+                if(checkingUsernameOnValidity(user)) {
+                    DBConnection.getInstance().insertUserIntoDB(user);
                     openGameMenuActivity();
                 }
 
@@ -46,12 +45,12 @@ public class RegisterActivity extends AppCompatActivity {
         Toast.makeText(RegisterActivity.this, msg, Toast.LENGTH_LONG).show();
     }
 
-    private boolean checkingUsernameOnValidity(DBConnection connection, String username) {
-        boolean userExists = connection.checkUsername(username), validUser;
-        if (username.isEmpty() | username.equals("") | username.length() <= 3) {
+    private boolean checkingUsernameOnValidity(User user) {
+        boolean validUser;
+        if (user.getName().isEmpty() | user.getName().equals("") | user.getName().length() <= 3) {
             openErrorMessage(getString(R.string.invalidUsernameLength));
             validUser = false;
-        } else if (userExists) {
+        } else if (DBConnection.getInstance().comparingUserUiWithUserDatabase(user)) {
             openErrorMessage(getString(R.string.usernameAlreadyUsed));
             validUser = false;
         } else {
@@ -77,4 +76,21 @@ public class RegisterActivity extends AppCompatActivity {
         return validPassword;
     }
 
+    private User readInputFields() {
+        EditText usernameTxtField = findViewById(R.id.usernameRegisterField);
+        EditText passwordTxtField = findViewById(R.id.passwordRegisterField);
+        EditText repasswordTxtField = findViewById(R.id.repasswordRegisterField);
+
+        String username = usernameTxtField.getText().toString();
+        String password = passwordTxtField.getText().toString();
+        String repassword = repasswordTxtField.getText().toString();
+        User user = new User();
+
+        if(checkingPasswordOnValidity(password, repassword)) {
+            user = new User(username, password);
+        } else {
+            Toast.makeText(RegisterActivity.this, "Please check that your passwords are the same", Toast.LENGTH_SHORT).show();
+        }
+        return user;
+    }
 }
